e8a487b6c471d225ae475b9675e63a8442d4049c
ahmedhakim0011 <ahmdahmed259@gmail.com>
2022-07-04 18:51:05 +0500

    new commit

---
+++ lib/signnig.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  State<SignIn> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  State<SignUp> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
